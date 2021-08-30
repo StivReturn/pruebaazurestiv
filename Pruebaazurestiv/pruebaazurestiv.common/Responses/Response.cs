13b29863b822f5527dc3c09f9a73e9e29bd3a8ab
STIVEN BALLESTEROS GARC�A <stivenballesteros123404@correo.itm.edu.co>
2021-08-30 18:12:22 -0500

    Add entity

---
+++ Pruebaazurestiv/pruebaazurestiv.common/Responses/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pruebaazurestiv.common.Responses
+{
+    class Response
+    {
+
+        public bool IsSucces { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result{ get; set; }
+
+    }
+}
